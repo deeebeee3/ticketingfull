ad439d6793dc0bf5cc01fbff6f68938654e7cc5b
Deepak Bhari <deepak.bhari@lifts.to>
2021-02-16 16:05:46 +0000

    determining error type

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 /* only requirement for error handling middleware is that 
 it recieves the following 4 arguments */
@@ -8,7 +10,13 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log(err.message);
+  if (err instanceof RequestValidationError) {
+    console.log("RequestValidationError");
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    console.log("DatabaseConnectionError");
+  }
 
   res.status(400).send({
     message: err.message,
