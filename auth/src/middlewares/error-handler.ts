dd1ccca31f56be4efa001d2dd35f20198a3dffd7
Deepak Bhari <deepak.bhari@lifts.to>
2021-02-16 16:22:43 +0000

    Convert Errors to standard response format

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -12,13 +12,27 @@ export const errorHandler = (
 ) => {
   if (err instanceof RequestValidationError) {
     console.log("RequestValidationError");
+
+    const formattedErrors = err.errors.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
+
+    return res.status(400).send({ errors: formattedErrors });
   }
 
   if (err instanceof DatabaseConnectionError) {
     console.log("DatabaseConnectionError");
+
+    return res.status(500).send({
+      errors: [{ message: err.reason }],
+    });
   }
 
   res.status(400).send({
-    message: err.message,
+    errors: [
+      {
+        message: "Something went wrong",
+      },
+    ],
   });
 };
