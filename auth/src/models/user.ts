48d7372a1537be7498e7d9ae42b7b4fdb39f8d58
Deepak Bhari <deepak.bhari@lifts.to>
2021-02-17 17:27:13 +0000

    add type checking and a static function

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
 
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -10,6 +19,12 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+//add custom function to statics property of schema
+//add type checking by specifying type
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
 
 const User = mongoose.model("User", userSchema);
 
