947288f7ed95a950088037ad2b69cd8a33de5384
Deepak Bhari <deepak.bhari@lifts.to>
2021-02-18 23:59:44 +0000

    Use current user middleware inside our current-user route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,24 +1,11 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 
+import { currentUser } from "../middlewares/current-user";
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  //decode / extract data out of jwt using verify()
-  //! used after JWT_KEY - tell ts not to check if property defined - we already checked
-  //in index.ts...
-  try {
-    //verify() will throw an error if token invalid
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-
-    res.send({ currentUser: payload });
-  } catch (err) {
-    res.send({ currentUser: null });
-  }
+router.get("/api/users/currentuser", currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
