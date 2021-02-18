e15dd26b756bd00276282f3f10bebc05c7a8d785
Deepak Bhari <deepak.bhari@lifts.to>
2021-02-18 17:25:00 +0000

    Format our JSON properties that we get back for a user

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -21,16 +21,28 @@ interface UserDocument extends mongoose.Document {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id; //remap _id to id
+        delete ret._id; //remove _id property from response
+        delete ret.password; //remove password property from response
+        delete ret.__v; //remove __v property from response
+      },
+    },
+  }
+);
 
 //this will always run before saving a Document or db
 //**don't use arrow function for callback, otherwise value of 'this' will be
