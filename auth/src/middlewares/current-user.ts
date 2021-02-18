e2ef9912024d26c94954fc4a1942b726fd2d45d5
Deepak Bhari <deepak.bhari@lifts.to>
2021-02-18 23:54:20 +0000

    Augmenting Type definitions, for our Current User middleware

--- auth/src/middlewares/current-user.ts
+++ auth/src/middlewares/current-user.ts
@@ -1,6 +1,22 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
+//We use this interface to explicitly tell ts what const payloads type is (line 38)
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
+//Augment type definition... reach into Expresses type definition and modify the
+//Request interface to have an optional property currentUser of type UserPayload
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
 export const currentUser = (
   req: Request,
   res: Response,
@@ -16,10 +32,13 @@ export const currentUser = (
   //in index.ts...
   try {
     //verify() will throw an error if token invalid
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_KEY!
+    ) as UserPayload;
 
     //we want to add a new property (currentUser) to req...
-    req.currentuser = payload;
+    req.currentUser = payload;
   } catch (err) {}
 
   next();
