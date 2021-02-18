18438271d65868e276ff6ffc880435b32e53fa8e
Deepak Bhari <deepak.bhari@lifts.to>
2021-02-18 23:26:10 +0000

    Returning the current signed in user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,24 @@
 import express from "express";
+import jwt, { verify } from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.send("hi there");
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  //decode / extract data out of jwt using verify()
+  //! used after JWT_KEY - tell ts not to check if property defined - we already checked
+  //in index.ts...
+  try {
+    //verify() will throw an error if token invalid
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+
+    res.send({ currentUser: payload });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
