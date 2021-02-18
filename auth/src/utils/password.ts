2ee88dae4e4e416257f9d08f2b61013f1712b9a1
Deepak Bhari <deepak.bhari@lifts.to>
2021-02-18 12:39:04 +0000

    Adding a Password class with hash and compare functions

---
+++ auth/src/utils/password.ts
@@ -0,0 +1,29 @@
+import { scrypt, randomBytes } from "crypto";
+import { promisify } from "util";
+
+//convert scrypt from callback based implementation to promise based implementation
+const scryptAsync = promisify(scrypt);
+
+export class Password {
+  static async toHash(password: string) {
+    const salt = randomBytes(8).toString("hex");
+
+    //ts doesn't know what buffer is (doesn't know what happened during promisify process)
+    //so just tell it to treat it as Buffer
+    const buffer = (await scryptAsync(password, salt, 64)) as Buffer;
+
+    //a buffer is like an array with raw data inside of it
+    return `${buffer.toString("hex")}.${salt}`;
+  }
+
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    //get stored hashed password in db
+    const [hashedPassword, salt] = storedPassword.split(".");
+
+    //hash the supplied password
+    const buffer = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    //compare the two
+    return buffer.toString("hex") === hashedPassword;
+  }
+}
