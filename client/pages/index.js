d936ffa51ddaea1b23ecd51a62ec9fc9559155f7
Deepak Bhari <deepak.bhari@lifts.to>
2021-02-24 13:45:37 +0000

    Useful info on why request from getInitialProps doesn't work

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,16 @@
-export default () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
+
+//next.js will call this func, while it is attempting to render the application
+//on the server one time. We can use it to fetch some initial data the component needs.
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get("/api/users/currentuser");
+
+  return response.data;
+};
+
+export default LandingPage;
