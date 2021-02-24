34c457efd3dbc3a33f5e4a24937eb0d8b1c958bc
Deepak Bhari <deepak.bhari@lifts.to>
2021-02-24 15:24:42 +0000

    Are we on the Server or the Browser

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,6 +8,20 @@ const LandingPage = ({ currentUser }) => {
 //next.js will call this func, while it is attempting to render the application
 //on the server one time. We can use it to fetch some initial data the component needs.
 LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    /*     
+      we are on the server
+      requests should be made like: 
+      http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser 
+    */
+  } else {
+    /*     
+      we are on the browser (browser will figure out domain)
+      requests should be made like: 
+      /api/users/currentuser 
+    */
+  }
+
   const response = await axios.get("/api/users/currentuser");
 
   return response.data;
