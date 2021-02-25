ed92e473a2dc28789dd4bdf42721912c844c0bd2
Deepak Bhari <deepak.bhari@lifts.to>
2021-02-25 20:49:14 +0000

    Use our published npm package inside auth and update import statements

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -4,8 +4,7 @@ import jwt from "jsonwebtoken";
 
 import { Password } from "../utils/password";
 import { User } from "../models/user";
-import { validateRequest } from "../middlewares/validate-request";
-import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest, BadRequestError } from "@dbtickets/common";
 
 const router = express.Router();
 
