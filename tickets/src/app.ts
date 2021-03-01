b5451d3b4815f91fbc0cc9a82519b2862f07d27a
Deepak Bhari <deepak.bhari@lifts.to>
2021-03-01 21:39:38 +0000

    Add test and route implementation to get ALL tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@dbtickets/common";
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
 
 const app = express();
 app.set("trust proxy", true); //trust traffic coming from our ingress-nginx proxy
@@ -20,6 +21,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
