48df2025e543ac13caa046e4207f263cbb7ac2a7
Deepak Bhari <deepak.bhari@lifts.to>
2021-03-01 22:29:24 +0000

    Update a ticket

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -74,4 +74,30 @@ it("returns a 400 if the user provides an invalid title or price", async () => {
     .expect(400);
 });
 
-it("updates the ticket if provided valid inputs", async () => {});
+it("updates the ticket if provided valid inputs", async () => {
+  const cookie = global.createFakeCookie();
+
+  const response = await request(app)
+    .post(`/api/tickets`)
+    .set("Cookie", cookie)
+    .send({
+      title: "ffwiufiw",
+      price: 20,
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "Valid Title and Price",
+      price: 17,
+    })
+    .expect(200);
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+
+  expect(ticketResponse.body.title).toEqual("Valid Title and Price");
+  expect(ticketResponse.body.price).toEqual(17);
+});
