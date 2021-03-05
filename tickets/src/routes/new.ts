db900c4527dc20ec0ef56bec773eb661ffe4b1b7
Deepak Bhari <deepak.bhari@lifts.to>
2021-03-05 17:17:28 +0000

    Publishing Updated Ticket event

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,7 +3,6 @@ import { body } from "express-validator";
 import { requireAuth, validateRequest } from "@ddbtickets/common";
 import { Ticket } from "../models/ticket";
 import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
-
 import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
@@ -28,6 +27,7 @@ router.post(
     });
 
     await ticket.save();
+
     new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
