fed63ce0da52607223aaa5da0661849df3a4ec7b
Deepak Bhari <deepak.bhari@lifts.to>
2021-03-10 15:54:16 +0000

    Test - asserting reserved tickets / testing the success case

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../../app";
+import { Order, OrderStatus } from "../../models/order";
+import { Ticket } from "../../models/ticket";
 
 it("returns an error if the ticket does not exist", async () => {
   const ticketId = mongoose.Types.ObjectId();
@@ -14,6 +16,41 @@ it("returns an error if the ticket does not exist", async () => {
     .expect(404);
 });
 
-it("returns an error if the ticket is already reserved", () => {});
+it("returns an error if the ticket is already reserved", async () => {
+  const ticket = Ticket.build({
+    title: "Concert",
+    price: 20,
+  });
 
-it("reserves a ticket", () => {});
+  await ticket.save();
+
+  const order = Order.build({
+    ticket,
+    userId: "iefiheiivne",
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+
+  await order.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.createFakeCookie()) //signin
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
+
+it("reserves a ticket", async () => {
+  const ticket = Ticket.build({
+    title: "Concert",
+    price: 20,
+  });
+
+  await ticket.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.createFakeCookie()) //signin
+    .send({ ticketId: ticket.id })
+    .expect(201);
+});
