3b8d675623209debcfe6cbbf3b82b268ee4fdbde
Deepak Bhari <deepak.bhari@lifts.to>
2021-03-10 17:25:45 +0000

    Cancelling an Order - should be patch but we used delete

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,36 @@
 import express, { Request, Response } from "express";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@ddbtickets/common";
+import { Order, OrderStatus } from "../models/order";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    //set status to cancelled
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    //204 is for successfully delete - we are not really deleting but cancelling an order
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
